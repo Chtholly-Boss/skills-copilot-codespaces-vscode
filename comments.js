d8a72089eb5db3f88ae2f97a5382408b66ad1e55
Yibin Ma <220110915@stu.hit.edu.cn>
2024-12-09 17:53:06 +0000

    comments.js done

---
+++ comments.js
@@ -0,0 +1,14 @@
+// create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// create a route
+app.get('/comments/:id', (req, res) => {
+  res.send('You requested a comment whose id is ' + req.params.id);
+});
+
+// start server
+app.listen(port, () => {
+  console.log('Server started at http://localhost:' + port);
+});
